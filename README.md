502f4ee6cbcee7180e5b1e0cf49342d562704f52
av-vlasov <skazochmik2007@gmail.com>
2019-07-03 12:55:18 +0300

    work version

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # apache-mon
+# autor av-vlasov
+## Variable ##
+### Command ###
+ECHO = echo -e 
+TIME = YYYY-mm-dd_HH:MM:SS - дата и время
+DIR = `pwd` - текущая директория
+HOST = адрес для проверки
+## Function ##
+### send_mail ###
+ отправка почты всем из списка MAIL_LIST
+### apache_start ###
+ старт сервиса apache 
+ после старта запускает функцию send_mail
+### apache_stop ###
+ остановка apache, через kill -9 на случай повисших процессов 
+ после выполнения запускает функцию apache_start.
+### check_apache ###
+ проверка доступности $HOST, в случае отсутствия отклика 200 или 301 создает log файл с текущей датой;
+ запускает функцию apache_stop
+### while ###
+ каждые 5 секунд запускает функцию check_apache 
---
+++ checker_apache.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+### Variable ###
+# Comand #
+ECHO="$(which echo) -e"
+TIME="date +%F_%T"
+# Main #
+DIR=$(pwd)
+# User vars #
+HOST='localhost'
+###
+send_mail () {
+    LOG=$1
+    MAIL_LIST=""
+    for MAIL in $MAIL_LIST
+    do
+        cat $LOG | mail -s "$($TIME) apache restart in $(hostname)" $MAIL
+    done
+}
+apache_start () {
+    LOG=$1
+    if [[ -n $(systemctl status apache2.service | grep active) ]]
+    then
+        systemctl start apache2.service \
+        && $ECHO "Apache start in $($TIME)\n==========\n" >> $LOG \
+        || $ECHO "ALARM! Apache not started in $($TIME)\n==========\n" >> $LOG 
+    fi
+    send_mail "$LOG"
+}
+apache_stop () {
+    LOG=$1
+    $ECHO "netstat result:\n" >> $LOG
+    netstat -ne | grep ":80\|:443" >> $LOG
+    $ECHO "-----------\napache processes:\n" >> $LOG
+    ps aux | grep apache2 | grep -v grep >> $LOG
+    for ID in $( ps aux | grep apache2 | grep -v grep | awk '{print$2}' )
+    do
+        kill -9 $ID 2>/dev/null
+    done
+    apache_start "$LOG"
+}
+check_apache () {
+    if [[ -z $(curl -I $HOST | grep "200\|301") ]] 
+    then
+        LOG="$($TIME)_apache_mon.log"
+        $ECHO "$($TIME) - $HOST not available\n---------------" >> $LOG
+        apache_stop "$LOG"
+    fi
+}
+while :; do
+    check_apache
+    sleep 5
+done
